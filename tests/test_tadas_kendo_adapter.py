6943bc732df34bbf0756625a9bf9de58db6c415c
FaramarzKowsari <105053743+FaramarzKowsari@users.noreply.github.com>
2026-08-19 21:32:25 +0300

    Test keyboard-driven Kendo date commit sequence

--- tests/test_tadas_kendo_adapter.py
+++ tests/test_tadas_kendo_adapter.py
@@ -5,6 +5,7 @@ from scripts.tadas_kendo_adapter import (
     DATE_INPUT_SELECTOR,
     EVENT_ID_SELECTOR,
     assert_preserved_value,
+    keyboard_commit_kendo_date,
 )
 
 
@@ -25,3 +26,30 @@ def test_preserved_value_guard_rejects_kendo_date_reset():
             "17-08-2026 18:07:36",
             "18-03-2024 00:00:00",
         )
+
+
+class FakeLocator:
+    def __init__(self):
+        self.calls = []
+
+    def click(self):
+        self.calls.append(("click",))
+
+    def press(self, key):
+        self.calls.append(("press", key))
+
+    def press_sequentially(self, text, delay=0):
+        self.calls.append(("press_sequentially", text, delay))
+
+
+def test_kendo_date_commit_uses_real_keyboard_event_path():
+    locator = FakeLocator()
+    keyboard_commit_kendo_date(locator, "18-03-2024 00:00:00", delay_ms=7)
+    assert locator.calls == [
+        ("click",),
+        ("press", "Control+A"),
+        ("press", "Backspace"),
+        ("press_sequentially", "18-03-2024 00:00:00", 7),
+        ("press", "Enter"),
+        ("press", "Tab"),
+    ]
